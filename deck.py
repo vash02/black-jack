b3347a731124fe12433196a0decaff4169b2b902
Vaibhav Sharma <vaibhav@Vaibhavs-MacBook-Air.local>
2024-01-06 16:45:44 +0530

    Black jack game

---
+++ deck.py
@@ -0,0 +1,24 @@
+from Utils import *
+import random
+from card import Card
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
